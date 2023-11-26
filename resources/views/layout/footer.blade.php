0c09a3c02bc92306d0ccb6447f9f7b63510bd603
arifbillah112 <arifbillah32231@gmail.com>
2023-11-27 00:30:10 +0600

    Finla Commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,12 @@
+  {{-- <-- ======= Footer ======= --> --}}
+  <footer id="footer">
+      <div class="container">
+          <div class="copyright">
+              &copy; Copyright <strong><span>iPortfolio</span></strong>
+          </div>
+          <div class="credits">
+            Portfolio of <i>Arif Billah</i>
+          </div>
+      </div>
+  </footer>
+  {{-- <-- End  Footer --> --}}
